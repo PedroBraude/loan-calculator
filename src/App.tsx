460ff63be5ee80e1219b8300f9744ab70a76838f
Pedro Braude <pedrobraude@gmail.com>
2024-04-25 08:47:53 -0300

    feat: add UI 'select months' dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import { formatMoney } from './utils'
 
 export default function App() {
   const [quantity, setQuantity] = useState(10000) // returns an array. This is why we use destructuring.
+  const [months, setMonths] = useState(6)
 
   const MIN = 0
   const MAX = 20000
   const STEP = 100
 
-  function handleChangeRangeInput(
-    e: React.ChangeEvent<HTMLInputElement>
-  ) {
+  function handleChangeRangeInput(e: React.ChangeEvent<HTMLInputElement>) {
     setQuantity(+e.target.value)
   }
 
@@ -47,6 +46,20 @@ export default function App() {
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatMoney(quantity, 'USD', 'en-US')}
       </p>
+
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className="text-indigo-600"> Plazo</span> a pagar
+      </h2>
+
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={months}
+        onChange={e => setMonths(+e.target.value)}
+      >
+        <option value="6">6 meses</option>
+        <option value="12">12 meses</option>
+        <option value="24">24 meses</option>
+      </select>
     </div>
   )
 }
